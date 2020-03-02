e539d2b2912da40151ea4c5f9cfe2e6bb80b75c7
Luiz Pontes <eldorado.luiz@gmail.com>
2020-03-01 21:42:28 -0300

    Updated README, added database SQL

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # uno-telegram-bot
 A UNO Telegram bot, currently on [@zeroxisbot](https://t.me/zeroxisbot). Made to be hosted on Heroku.
+
+## How to host
+
+* Install the Python `requeriments.txt` with `pip install -r requirements.txt`. In Heroku that will be done automatically.
+
+* Install PostgreSQL, set up a database and run `psql < database.sql`. In Heroku, you may install the Heroku Postgres add-on, and run `heroku pg:psql < database.sql`.
+
+* Set up a bot in BotFather.
+
+* Set up these enviroment variables in your server:
+	* `TELEGRAM_BOT_TOKEN`: The Telegram bot's token, taken from BotFather.
+	* `TELEGRAM_BOT_WEBHOOK`: The public URL of your server, with a trailing slash.
+	* `DATABASE_URL`: A database URL containing the connection info, with the format `postgresql://user:password@host:port/databasename`. The Heroku Postgres add-on automatically creates this variable.
+	* `PORT`: The host port that the public URL is listening from. Heroku will automatically create this variable.
+
+* Start `main.py` file to host the bot. In Heroku, `Procfile` will take care of that.
---
+++ database.sql
@@ -0,0 +1,126 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.2
+-- Dumped by pg_dump version 12.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+ALTER TABLE ONLY public.uno_rooms DROP CONSTRAINT uno_rooms_pkey;
+ALTER TABLE ONLY public.uno_joins DROP CONSTRAINT uno_joins_pkey;
+ALTER TABLE public.uno_rooms ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.uno_joins ALTER COLUMN id DROP DEFAULT;
+DROP SEQUENCE public.uno_rooms_id_seq;
+DROP TABLE public.uno_rooms;
+DROP SEQUENCE public.uno_joins_id_seq;
+DROP TABLE public.uno_joins;
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: uno_joins; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.uno_joins (
+    id integer NOT NULL,
+    room_id integer,
+    user_id integer,
+    player_number integer
+);
+
+
+--
+-- Name: uno_joins_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.uno_joins_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: uno_joins_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.uno_joins_id_seq OWNED BY public.uno_joins.id;
+
+
+--
+-- Name: uno_rooms; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.uno_rooms (
+    id integer NOT NULL,
+    game_pickle bytea
+);
+
+
+--
+-- Name: uno_rooms_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.uno_rooms_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: uno_rooms_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.uno_rooms_id_seq OWNED BY public.uno_rooms.id;
+
+
+--
+-- Name: uno_joins id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.uno_joins ALTER COLUMN id SET DEFAULT nextval('public.uno_joins_id_seq'::regclass);
+
+
+--
+-- Name: uno_rooms id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.uno_rooms ALTER COLUMN id SET DEFAULT nextval('public.uno_rooms_id_seq'::regclass);
+
+
+--
+-- Name: uno_joins uno_joins_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.uno_joins
+    ADD CONSTRAINT uno_joins_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: uno_rooms uno_rooms_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.uno_rooms
+    ADD CONSTRAINT uno_rooms_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
